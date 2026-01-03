a50757809f3aa225d5289cd2800982f26a4f4a3b
qiadastrachen-bit <qiadastrachen@gmail.com>
2026-01-04 00:27:19 +0800

    Add files via upload

---
+++ mser/models/__init__.py
@@ -0,0 +1,16 @@
+import importlib
+
+from loguru import logger
+from .base_model import BaseModel
+
+__all__ = ['build_model']
+
+
+def build_model(input_size, configs):
+    use_model = configs.model_conf.get('model', 'BaseModel')
+    print(use_model)
+    model_args = configs.model_conf.get('model_args', {})
+    mod = importlib.import_module(__name__)
+    model = getattr(mod, use_model)(input_size=input_size, **model_args)
+    logger.info(f'成功创建模型：{use_model}，参数为：{model_args}')
+    return model
